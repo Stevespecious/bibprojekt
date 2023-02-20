a4d8267d6f9a142bed2038dc5f20168d9967cc44
Stevespecious <g4m3rkc@gmail.com>
2023-02-20 10:11:15 +0100

    laver getString

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getString("hvad hedder du?"));
 
     }
 
@@ -10,4 +11,5 @@ public class Main {
         System.out.println(s + " : ");
         return scanner.nextLine();
     }
+
 }
