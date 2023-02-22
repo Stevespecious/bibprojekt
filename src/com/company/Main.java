c247f022270067abf88b0c4e8cbcc53f8a53345e
Stevespecious <g4m3rkc@gmail.com>
2023-02-22 12:37:53 +0100

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,57 @@
 package com.company;
 
-import java.util.Scanner;
+import database.ConnectionConfiguration;
+import entitet.Låner;
+import utilities.input;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getString("hvad hedder du?"));
-        System.out.println(getInt("angiv venligst din skostørrelse?"));
 
-    }
+        ConnectionConfiguration.getConnection();
+        List<Låner> lånerList = new LinkedList<>();
 
-    public static String getString(String s) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(s + " : ");
-        return scanner.nextLine();
-    }
+        //try {
+            //Connection connection = ConnectionConfiguration.getConnection();
 
-    public static int getInt(String s) {
-        int res = 0;
+                //PreparedStatement statement = connection.prepareStatement("select * from bibliotekOnsdag.låner");
+                //ResultSet result = statement.executeQuery();
 
+            //while (result.next()) {
+
+                //int id = result.getInt("idlåner");
+                //String navn = result.getString("navn");
+                //String adresse = result.getString("adresse");
+                //int postnummer = result.getInt("postnummer");
+
+                //Låner låner = new Låner(id, navn, adresse, postnummer);
+
+                //System.out.println(låner.toString());
+
+                //lånerList.add(låner);
+            //}
+
+        //} catch (Exception e) {
+            //e.printStackTrace();
+        //}
+
+        for(Låner låner : lånerList) {
+            System.out.println(låner.toString());
+        }
 
         while (true) {
-            try {
-            res = Integer.parseInt(getString(s));
-            return res;
-
-            } catch (Exception e) {
-                System.out.println("husk ikke tal-ord");
+            switch (input.getString("angiv dit ønske?")) {
+                case "lån bog":
+                    System.out.println("du ønsker at låne en bog");
+                    break;
+                case "stop":
+                    break;
+            }
+            if (input.getString("q for quit").equalsIgnoreCase("q")) {
+                break;
             }
         }
     }
